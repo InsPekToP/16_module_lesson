deedef64be05a65641ce54398695ae1efb7d287a
IsPekToP <inspektop1@gmail.com>
2024-07-17 12:16:47 +0200

    добаил 16maim3_1

---
+++ 16main3_1/itProger/users/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render,redirect
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+#создавать разные типы сообщений
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+
+
+# def register(request):
+#     form = UserCreationForm()
+#     return render(
+#         request,
+#         'users/registration.html',
+#         {
+#             'title':'Страница регистрации',
+#             'form':form
+#             })
+
+#теперь вместо стандартного класса UserCreationForm подставляем свой UserRegisterForm
+
+def register(request):
+    #если данные передаются методом post
+    if request.method == "POST":
+        #здесть будут храниться все данные полученные от польз-ля
+        form = UserRegisterForm(request.POST)
+        #проверка на правильность данных
+        if form.is_valid():
+#чтобы сохранять зарегистрированых пользователей в БД надо эту строчку раскоментить
+            form.save()
+            #очищенные данные(не понятно)
+            username = form.cleaned_data.get('username')
+            #messages.info()
+            # messages.error()
+            # messages.warning()
+            messages.success(request,f'Пользователь {username} был успешно создан!')
+            #теперь надо перебросить поль-ля на другую страницу
+            return redirect('home')
+
+    else:
+        form = UserRegisterForm()
+    return render(
+        request,
+        'users/registration.html',
+        {
+            'title':'Страница регистрации',
+            'form':form
+            }
+        )
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
